ce2541fc5931b0c3b96805dab5de7b97e145df63
Habiba <habebaesam13@gmail.com>
2025-06-23 21:27:01 +0300

    product index/show

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Traits\ApiResponseTrait;
+use App\Http\Controllers\Controller;
+
 class ProductsController extends Controller
 {
     use ApiResponseTrait;
     public function index(){
-        
+        $products = Product::all();
+
+    return $this->successResponse([
+        'products' => $products
+    ],'Products retrived successfully',200);
     }
+
+public function show(Product $product)
+{
+    return $this->successResponse([
+        'product' => $product
+    ], 'Product retrieved successfully',
+200);
+}
 }
